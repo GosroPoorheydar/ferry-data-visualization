bf4cb153953eabdf6e323ff7cb74b3a67816f94c
bvictorb <victory-boucher@hotmail.com>
2020-12-04 02:07:20 +0100

    added overlay

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,14 +1,23 @@
 import { useState } from 'react'
+import people from '../img/people.png'
 
 const Overlay = () => {
     const [showOverlay, setShowOverlay] = useState(false)
 
+    if (showOverlay) {
+        document.body.style.overflow = 'hidden'
+    } else {
+        document.body.style.overflow = 'auto'
+    }
+
     return (
         <div className={showOverlay ? "m-overlay" : "m-overlay m-overlay--hide"}>
             <div className="m-overlay__container">
-                <h1>Lorem ipsum dolor sit amet.</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus commodi aliquam rem ab, reiciendis eum. Obcaecati illum labore accusantium accusamus illo explicabo ad eos animi voluptates dignissimos! Dolor, minus eum.</p>
-                <button onClick={() => setShowOverlay(false)} className="m-overlay__button">GO!</button>
+                <h1>MASK ON,<br></br> F*CK IT MASK OFF</h1>
+                <h2>A data visualization about the ferries in Amsterdam.</h2>
+                <img src={people} alt=""/>
+                <p>(one in five people doesn't wear a when leaving the ferry)</p>
+                <button onClick={() => setShowOverlay(false)} className="m-overlay__button">Show me the data!</button>
             </div>
         </div>
     )
